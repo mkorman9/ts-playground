1a28010cb9a896737b027470909081e4bac2e25a
Michal Korman <michal.korman@icloud.com>
2023-07-24 19:29:30 +0200

    ZodType -> z.Schema

--- src/request_body.ts
+++ src/request_body.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
-import { ZodType } from 'zod';
+import z from 'zod';
 import { validationResult } from 'express-validator';
 
 interface RequestWithValidatedBody extends Request {
   validatedBody: unknown;
 }
 
-export const bindRequestBody = (t: ZodType) => {
+export const bindRequestBody = (schema: z.Schema) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const validationErrors = validationResult(req);
     if (!validationErrors.isEmpty()) {
@@ -20,7 +20,7 @@ export const bindRequestBody = (t: ZodType) => {
     }
 
     try {
-      (req as RequestWithValidatedBody).validatedBody = t.parse(req.body);
+      (req as RequestWithValidatedBody).validatedBody = schema.parse(req.body);
     } catch (err) {
       return res.status(400).json({
         error: 'Malformed request payload',
