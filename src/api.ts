90b172b29f725aa027670e26ef4b6569c422a539
Michal Korman <michal.korman@icloud.com>
2023-07-26 01:20:34 +0200

    extract api to separate module

---
+++ src/api.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import { z } from 'zod';
+import { bindRequestBody, getRequestBody } from './request_body';
+import { findPublicIp } from './ip';
+
+const api = Router();
+
+const PayloadRequestSchema = z.object({
+  name: z.string(),
+  age: z.number().min(1),
+  timestamp: z.coerce.date()
+});
+
+type PayloadRequest = z.infer<typeof PayloadRequestSchema>;
+
+api.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    hello: 'world!'
+  });
+});
+
+api.get('/ip', (req: Request, res: Response, next: NextFunction) => {
+  findPublicIp()
+    .then(ip => {
+      res.status(200).json({
+        ip: ip
+      });
+    })
+    .catch(err => next(err));
+});
+
+api.put('/payload', bindRequestBody(PayloadRequestSchema), (req: Request, res: Response) => {
+  const body = getRequestBody<PayloadRequest>(req);
+  res.status(200).json(body);
+});
+
+export default api;
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { findPublicIp } from './ip';
-import { getRequestBody, bindRequestBody } from './request_body';
-import { z } from 'zod';
+import api from './api';
 
 const app = express();
 
@@ -24,34 +22,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   next(err);
 });
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({
-    hello: 'world!'
-  });
-});
-
-app.get('/ip', (req: Request, res: Response, next: NextFunction) => {
-  findPublicIp()
-    .then(ip => {
-      res.status(200).json({
-        ip: ip
-      });
-    })
-    .catch(err => next(err));
-});
-
-const PayloadRequestSchema = z.object({
-  name: z.string(),
-  age: z.number().min(1),
-  timestamp: z.coerce.date()
-});
-
-type PayloadRequest = z.infer<typeof PayloadRequestSchema>;
-
-app.put('/payload', bindRequestBody(PayloadRequestSchema), (req: Request, res: Response) => {
-  const body = getRequestBody<PayloadRequest>(req);
-  res.status(200).json(body);
-});
+app.use('/', api);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (res.headersSent) {
