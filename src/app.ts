d6ed1a3b827d1700ac2052f5efed9db7edb3cf11
Michal Korman <michal.korman@icloud.com>
2023-07-26 01:09:51 +0200

    add custom error handlers

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,20 @@ app.disable('x-powered-by');
 app.disable('etag');
 app.use(express.json());
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  if (err instanceof SyntaxError && 'body' in err) {
+    return res.status(400).json({
+      error: 'Request parsing error',
+    });
+  }
+
+  next(err);
+});
+
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     hello: 'world!'
@@ -37,6 +51,18 @@ type PayloadRequest = z.infer<typeof PayloadRequestSchema>;
 app.put('/payload', bindRequestBody(PayloadRequestSchema), (req: Request, res: Response) => {
   const body = getRequestBody<PayloadRequest>(req);
   res.status(200).json(body);
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  
+  console.error(err);
+
+  res.status(500).json({
+    error: 'Internal server error',
+  });
 });
 
 export default app;
