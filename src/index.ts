5162b7a957825c2486e8559c38d1e9bc01148ef2
Michal Korman <michal.korman@icloud.com>
2023-07-29 13:33:44 +0200

    rework app startup and shutdown

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,21 @@ import app from './app';
 import log from './providers/log';
 
 const server = app.listen(config.HTTP_PORT, config.HTTP_HOST, () => {
-  log.info(`Listening on ${config.HTTP_HOST}:${config.HTTP_PORT}`);
+  log.info(`Server started listening on ${config.HTTP_HOST}:${config.HTTP_PORT}`);
+});
+
+server.on('error', err => {
+  log.error('Failed to start up the server', { stack: err });
+  process.exit(1);
 });
 
 process.on('SIGINT', () => {
+  if (!server.listening) {
+    return;
+  }
+
   server.close(() => {
-    log.info('Closed server');
+    log.info('Server shutdown complete');
     process.exit(0);
   });
 
