103969558ce7b8fe1bb4e2527c2706798df40a19
Michal Korman <michal.korman@icloud.com>
2023-07-31 20:47:40 +0200

    remove dead code and introduce api tests

---
+++ src/api.spec.ts
@@ -0,0 +1,17 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from './app';
+
+chai.use(chaiHttp).should();
+
+describe('API', () => {
+  test('it should return 200 when calling /health/readiness', async () => {
+    const response = await chai.request(app).get('/health/readiness');
+    expect(response.statusCode).toEqual(200);
+  });
+
+  test('it should return 200 when calling /health/liveness', async () => {
+    const response = await chai.request(app).get('/health/liveness');
+    expect(response.statusCode).toEqual(200);
+  });
+});
