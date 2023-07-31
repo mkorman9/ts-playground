6b9fb9b6dd812ee5b8d69e804c898f165e8d4921
Michal Korman <michal.korman@icloud.com>
2023-07-31 20:49:21 +0200

    simplify

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -2,7 +2,7 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from './app';
 
-chai.use(chaiHttp).should();
+chai.use(chaiHttp);
 
 describe('API', () => {
   test('it should return 200 when calling /health/readiness', async () => {
