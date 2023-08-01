f81d8b436c5b4fb061bf3a591b7a637802880493
Michal Korman <michal.korman@icloud.com>
2023-08-01 17:35:10 +0200

    change jest syntax

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -6,27 +6,27 @@ import dayjs from 'dayjs';
 chai.use(chaiHttp);
 
 describe('API', () => {
-  test('it should return 200 when calling GET /', async () => {
+  it('should return 200 when calling GET /', async () => {
     const response = await chai.request(app).get('/');
     expect(response.statusCode).toEqual(200);
   });
 
-  test('it should return 200 when calling GET /health/readiness', async () => {
+  it('should return 200 when calling GET /health/readiness', async () => {
     const response = await chai.request(app).get('/health/readiness');
     expect(response.statusCode).toEqual(200);
   });
 
-  test('it should return 200 when calling GET /health/liveness', async () => {
+  it('should return 200 when calling GET /health/liveness', async () => {
     const response = await chai.request(app).get('/health/liveness');
     expect(response.statusCode).toEqual(200);
   });
 
-  test('it should return 500 when calling GET /error', async () => {
+  it('should return 500 when calling GET /error', async () => {
     const response = await chai.request(app).get('/error');
     expect(response.statusCode).toEqual(500);
   });
 
-  test('it should return 200 when sending valid payload to PUT /payload', async () => {
+  it('should return 200 when sending valid payload to PUT /payload', async () => {
     const payload = {
       name: 'John Doe',
       age: 37,
@@ -41,7 +41,7 @@ describe('API', () => {
     expect(response.body).toEqual(payload);
   });
 
-  test('it should return 400 when sending invalid payload to PUT /payload', async () => {
+  it('should return 400 when sending invalid payload to PUT /payload', async () => {
     const response = await chai.request(app)
       .put('/payload')
       .set('Content-Type', 'application/json')
