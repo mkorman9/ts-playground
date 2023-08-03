086cc8a724ccc8274d81aea4bff42df9c3225015
Michal Korman <michal.korman@icloud.com>
2023-08-03 22:53:59 +0200

    lint

--- src/api/api.spec.ts
+++ src/api/api.spec.ts
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../app';
 import dayjs from 'dayjs';
+import app from '../app';
 import * as ip from '../providers/ip';
 
 chai.use(chaiHttp);
